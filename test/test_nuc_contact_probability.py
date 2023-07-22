30c8169af2e153d81ce250f1c6937788ca01c217
yuanting <yuantingma@189.cn>
2023-07-22 15:26:32 +0800

    Revert "upload"
    This reverts commit 79ff21ebd5aceca1a3f4af8ffd56658e46500c60.

--- test/test_nuc_contact_probability.py
+++
@@ -1,9 +0,0 @@
-from nuc_processing.NucContactMap import *
-
-import pytest
-
-
-def test_help():
-    with pytest.raises(SystemExit) as e:
-        main(["--help"])
-        assert e.value.code == 0

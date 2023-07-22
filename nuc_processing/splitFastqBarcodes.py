be906eecb0ea5911786f147062def1441e9b64ff
yuanting <yuantingma@189.cn>
2023-07-22 15:30:10 +0800

    upload

---
+++ nuc_processing/splitFastqBarcodes.py
@@ -0,0 +1,111 @@
+import os, sys
+
+from common import open_file_r
+
+
+def readNextFourLines(fp):
+
+  lines = []
+  for n in range(4):
+    line = fp.readline()
+    if not line:
+      return []
+    lines.append(line)
+
+  return lines
+
+
+def writeLines(fp, lines, barcodeLen):
+
+  n = barcodeLen + 1
+  fp.write(lines[0])
+  fp.write(lines[1][n:])
+  fp.write(lines[2])
+  fp.write(lines[3][n:])
+
+
+def splitBarcodes(inputFile1, inputFile2, outputDirectory, barcodeLen=3):
+
+  if not os.path.exists(outputDirectory):
+    os.makedirs(outputDirectory)
+
+  with open_file_r(inputFile1) as fpin1, open_file_r(inputFile2) as fpin2:
+    barcodeCount = {}
+    barcodeFp1 = {}
+    barcodeFp2 = {}
+    mismatched = 0
+    matched = 0
+    Ncount1 = 0
+    Ncount2 = 0
+
+    lines1 = readNextFourLines(fpin1)
+    while lines1:
+      if lines1[0][0] != '@':
+        raise Exception('lines1[0] does not start with @: "%s"' % lines1[0].strip)
+      lines2 = readNextFourLines(fpin2)
+      if lines2[0][0] != '@':
+        raise Exception('lines2[0] does not start with @: "%s"' % lines2[0].strip)
+
+      if lines1[1][barcodeLen] == 'N':
+        Ncount1 += 1
+
+      if lines2[1][barcodeLen] == 'N':
+        Ncount2 += 1
+
+      barcode1 = lines1[1][:barcodeLen]
+      barcode2 = lines2[1][:barcodeLen]
+      if barcode1 != barcode2:
+        mismatched += 1
+      else:
+        matched += 1
+        if barcode1 not in barcodeFp1:
+          fileName1 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile1)[:-3], barcode1))
+          fileName2 = os.path.join(outputDirectory, '%s_%s.fq' % (os.path.basename(inputFile2)[:-3], barcode2))
+          fpout1 = open(fileName1, 'w')
+          fpout2 = open(fileName2, 'w')
+          barcodeFp1[barcode1] = fpout1
+          barcodeFp2[barcode2] = fpout2
+          barcodeCount[barcode1] = 0
+
+        fpout1 = barcodeFp1[barcode1]
+        fpout2 = barcodeFp2[barcode2]
+        barcodeCount[barcode1] += 1
+
+        writeLines(fpout1, lines1, barcodeLen)
+        writeLines(fpout2, lines2, barcodeLen)
+
+      lines1 = readNextFourLines(fpin1)
+
+  # TODO: Can use ExitStack to manage files in Python3
+  for fpout1 in barcodeFp1.values():
+    fpout1.close()
+  for fpout2 in barcodeFp2.values():
+    fpout2.close()
+
+  total = matched + mismatched
+  print('total = %d' % total)
+  total = float(total)
+  print('matched = %d (%.1f%%)' % (matched, 100*matched/total))
+  print('mismatched = %d (%.1f%%)' % (mismatched, 100*mismatched/total))
+  print('N at fourth character in first file = %d (%.1f%%)' % (Ncount1, 100*Ncount1/total))
+  print('N at fourth character in second file = %d (%.1f%%)' % (Ncount2, 100*Ncount2/total))
+
+  for barcode in sorted(barcodeCount):
+    count = barcodeCount[barcode]
+    print('barcode "%s" = %d (%.1f%%)' % (barcode, count, 100*count/total))
+
+
+def main(argv=None):
+  if argv is None:
+    argv = sys.argv[1:]
+  in_file_1, in_file2 = argv[:2]
+  if len(argv) == 3:
+    out_dir = argv[2]
+  else:
+    out_dir = os.path.dirname(os.path.abspath(in_file_1))
+
+  splitBarcodes(in_file_1, in_file2, out_dir)
+
+
+if __name__ == '__main__':
+  main()
